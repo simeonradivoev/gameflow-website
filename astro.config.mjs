83f8c84a0424dfde9779a7067ba7f404dbc263df
Simeon Radivoev <simeonradivoev@gmail.com>
2026-05-06 23:35:09 +0300

    build: used env to specify base and url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simeonradivoev.github.io',
-    base: '/gameflow',
+    site: env.SITE_URL,
+    base: `/${env.SITE_BASE}`,
     vite: {
         plugins: [
             tailwindcss()
